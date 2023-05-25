9eb86da440a171f1dac813d97d5205eef1ead335
puly garcia <pulygarcia09@gmail.com>
2023-05-25 02:07:53 -0300

    agregando el fadeIn a las secciones con el forEach

--- js/app.js
+++ js/app.js
@@ -5,17 +5,9 @@ const cerrarHamburguesa = document.querySelector('.header__burguer-close svg');
 const nav = document.querySelector('.header__nav');
 
 //FADES
-const cardsProyectos = document.querySelector('.proyectos__grid'); 
+const hiddenElementos = document.querySelectorAll('.hidden');
 
-const conocimientosGrid = document.querySelector('.conocimientos__grid');
-
-const estudiosGrid = document.querySelector('.estudios__grid');
-
-const sobreMiGrid = document.querySelector('.sobre-mi__grid');
-
-const contacto = document.querySelector('.contacto__contenido');
-
-//eventos
+//Eventos
 addEvents();
 function addEvents(){
     hamburguesa.addEventListener('click', mostrarMenu);
@@ -33,30 +25,11 @@ function cerrarMenu(){
 }
 
 function fade(){
-    const rectProyectos = cardsProyectos.getBoundingClientRect();
-    const rectConocimientos = conocimientosGrid.getBoundingClientRect();
-    const rectEstudios = estudiosGrid.getBoundingClientRect();
-    const rectSobreMi = sobreMiGrid.getBoundingClientRect();
-    const rectContacto = contacto.getBoundingClientRect();
-
-    if(rectProyectos.y <= 1005.53125){
-        cardsProyectos.classList.remove('none');
-        cardsProyectos.classList.add('fadeInUp');
-    }
-
-    if(rectConocimientos.y <= 1005.53125){
-        conocimientosGrid.classList.add('fadeInUp');
-    }
-
-    if(rectEstudios.y <= 1005.53125){
-        estudiosGrid.classList.add('fadeInUp');
-    }
-
-    if(rectSobreMi.y <= 1005.53125){
-        sobreMiGrid.classList.add('fadeInUp');
-    }
-
-    if(rectContacto.y <= 1005.53125){
-        contacto.classList.add('fadeInUp');
-    }
+    hiddenElementos.forEach(elemento => {
+        const rect = elemento.getBoundingClientRect();
+        if(rect.y <= 1005.53125){
+            elemento.classList.remove('hidden');
+            elemento.classList.add('fadeInUp');
+        }
+    })
 }
